6851147f8081ab7bef740f7711dfde3094e377ed
MehemmedBulut <mehemmedbulut@gmail.com>
2023-12-11 01:32:12 +0400

    menu item

---
+++ TrainPro/Data/ApplicationDbContext.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TrainPro.Models;
+using static System.Net.WebRequestMethods;
+
+namespace TrainPro.Data
+{
+    public class ApplicationDbContext:IdentityDbContext<AppUser>
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<AppUser> AppUser { get; set; }
+        public DbSet<MenuItem> MenuItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<MenuItem>().HasData(
+            new MenuItem
+            {
+                    Id = 1,
+                    Name = "Spring Roll",
+                    Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                    Image = "https://exampleproject.blob.core.windows.net/res/spring roll.jpg",
+                    Price = 7.99,
+                    Category = "Appetizer",
+                    SpecialTag = ""
+                }, new MenuItem
+                {
+                    Id = 2,
+                    Name = "Idli",
+                    Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                    Image = "https://exampleproject.blob.core.windows.net/res/idli.jpg",
+                    Price = 8.99,
+                    Category = "Appetizer",
+                    SpecialTag = ""
+                }, new MenuItem
+                {
+                    Id = 3,
+                    Name = "Panu Puri",
+                    Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                    Image = "https://exampleproject.blob.core.windows.net/res/pani puri.jpg",
+                    Price = 8.99,
+                    Category = "Appetizer",
+                    SpecialTag = "Best Seller"
+                }, new MenuItem
+                {
+                    Id = 4,
+                    Name = "Hakka Noodles",
+                    Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                    Image = "https://exampleproject.blob.core.windows.net/res/hakka noodles.jpg",
+                    Price = 10.99,
+                    Category = "Entrée",
+                    SpecialTag = ""
+                }, new MenuItem
+                {
+                    Id = 5,
+                    Name = "Malai Kofta",
+                    Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                    Image = "https://exampleproject.blob.core.windows.net/res/malai kofta.jpg",
+                    Price = 12.99,
+                    Category = "Entrée",
+                    SpecialTag = "Top Rated"
+                }, new MenuItem
+                {
+                    Id = 6,
+                    Name = "Paneer Pizza",
+                    Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                    Image = "https://exampleproject.blob.core.windows.net/res/paneer pizza.jpg",
+                    Price = 11.99,
+                    Category = "Entrée",
+                    SpecialTag = ""
+                }, new MenuItem
+                {
+                    Id = 7,
+                    Name = "Paneer Tikka",
+                    Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                    Image = "https://exampleproject.blob.core.windows.net/res/paneer tikka.jpg",
+                    Price = 13.99,
+                    Category = "Entrée",
+                    SpecialTag = "Chef's Special"
+                }, new MenuItem
+                {
+                    Id = 8,
+                    Name = "Carrot Love",
+                    Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                    Image = "https://exampleproject.blob.core.windows.net/res/carrot love.jpg",
+                    Price = 4.99,
+                    Category = "Dessert",
+                    SpecialTag = ""
+                }, new MenuItem
+                {
+                    Id = 9,
+                    Name = "Rasmalai",
+                    Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                    Image = "https://exampleproject.blob.core.windows.net/res/rasmalai.jpg",
+                    Price = 4.99,
+                    Category = "Dessert",
+                    SpecialTag = "Chef's Special"
+                }, new MenuItem
+                {
+                    Id = 10,
+                    Name = "Sweet Rolls",
+                    Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                    Image = "https://exampleproject.blob.core.windows.net/res/sweet rolls.jpg",
+                    Price = 3.99,
+                    Category = "Dessert",
+                    SpecialTag = "Top Rated"
+                });
+        }
+    }
+}
--- TrainPro/Program.cs
+++ TrainPro/Program.cs
@@ -1,7 +1,21 @@
+using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TrainPro.Data;
+using TrainPro.Models;
+using TrainPro.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultDbConnection"));
+});
+builder.Services.AddSingleton(u => new BlobServiceClient(
+    builder.Configuration.GetConnectionString("StorageAccount")));
+builder.Services.AddSingleton<IBlobService, BlobService>();
+builder.Services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
