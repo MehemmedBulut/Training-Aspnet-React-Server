1067571b71fad8169cf4dcc5a6d0556e8aa4fa3e
MehemmedBulut <mehemmedbulut@gmail.com>
2023-12-14 22:33:58 +0400

    end changes

--- TrainPro/Controllers/ShoppingCartController.cs
+++ TrainPro/Controllers/ShoppingCartController.cs
@@ -23,15 +23,18 @@ namespace TrainPro.Controllers
         {
             try
             {
-                if(string.IsNullOrEmpty(userId))
+                ShoppingCart shoppingCart;
+                if (string.IsNullOrEmpty(userId))
                 {
-                    _response.IsSsuccess = false;
-                    _response.StatusCode = HttpStatusCode.BadRequest;
-                    return BadRequest(_response);
+                    shoppingCart = new();
                 }
-                ShoppingCart shoppingCart = _db.ShoppingCarts
-                    .Include(u=>u.CartItems).ThenInclude(u=>u.MenuItem)
-                    .FirstOrDefault(u=>u.UserId==userId);
+                else
+                {
+                   shoppingCart = _db.ShoppingCarts
+                    .Include(u => u.CartItems).ThenInclude(u => u.MenuItem)
+                    .FirstOrDefault(u => u.UserId == userId);
+                }
+                 
                 if(shoppingCart.CartTotal != null && shoppingCart.CartItems.Count > 0)
                 {
                     shoppingCart.CartTotal = shoppingCart.CartItems.Sum(u => u.Quantity * u.MenuItem.Price);
