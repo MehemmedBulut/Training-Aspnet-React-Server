755608a84c300e00d6a0956d21ad209f279df9d4
MehemmedBulut <mehemmedbulut@gmail.com>
2023-12-14 19:06:20 +0400

    addOrderHeaderAndDetal

---
+++ TrainPro/Models/OrderHeader.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TrainPro.Models
+{
+    public class OrderHeader
+    {
+        [Key]
+        public int OrderHeaderId { get; set; }
+        [Required]
+        public string PickUpName { get; set; }
+        [Required]
+        public string PickUpPhoneNumber { get; set; }
+        [Required]
+        public string PickUpEmail { get; set; }
+        public string ApplicationUserId { get; set; }
+        [ForeignKey("ApplicationUserId")]
+        public AppUser User { get; set; }
+        public string OrderTotal { get; set; }
+
+        public DateTime OrderDate { get; set; }
+        public string StripePaymentIntentId { get; set; }
+        public string Status { get; set; }
+        public int TotalItems { get; set; }
+        public IEnumerable<OrderDetails> OrderDetails { get; set; }
+    }
+}
