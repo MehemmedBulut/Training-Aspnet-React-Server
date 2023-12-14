df077b438c4b33b8b2786f9cb61082d3deb048f2
MehemmedBulut <mehemmedbulut@gmail.com>
2023-12-14 20:56:02 +0400

    CreateOrder

--- TrainPro/Controllers/OrderController.cs
+++ TrainPro/Controllers/OrderController.cs
@@ -4,7 +4,9 @@ using Microsoft.EntityFrameworkCore;
 using System.Net;
 using TrainPro.Data;
 using TrainPro.Models;
+using TrainPro.Models.Dto;
 using TrainPro.Services;
+using TrainPro.Utility;
 
 namespace TrainPro.Controllers
 {
@@ -78,6 +80,55 @@ namespace TrainPro.Controllers
                 _response.IsSsuccess = false;
                 _response.ErrorMessages
                     = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+        [HttpPost]
+        public async Task<ActionResult<ApiResponse>> CreateOrder([FromBody] OrderHeaderCreteDTO orderHeaderDTO)
+        {
+            try
+            {
+                OrderHeader order = new()
+                {
+                    ApplicationUserId = orderHeaderDTO.ApplicationUserId,
+                    PickUpEmail = orderHeaderDTO.PickUpEmail,
+                    PickUpName = orderHeaderDTO.PickUpName,
+                    PickUpPhoneNumber = orderHeaderDTO.PickUpPhoneNumber,
+                    OrderTotal = orderHeaderDTO.OrderTotal,
+                    OrderDate = DateTime.Now,
+                    StripePaymentIntentId = orderHeaderDTO.StripePaymentIntentId,
+                    TotalItems = orderHeaderDTO.TotalItems,
+                    Status = String.IsNullOrEmpty(orderHeaderDTO.Status)?SD.status_pending : orderHeaderDTO.Status,
+                };
+
+                if (ModelState.IsValid)
+                {
+                    _db.OrderHeaders.Add(order);
+                    _db.SaveChanges();
+                    foreach (var orderDetailDTO in orderHeaderDTO.OrderDetailsDTO)
+                    {
+                        OrderDetails orderDetails = new()
+                        {
+                            OrderHeaderId = order.OrderHeaderId,
+                            ItemName = orderDetailDTO.ItemName,
+                            MenuItemId = orderDetailDTO.MenuItemId,
+                            Price = orderDetailDTO.Price,
+                            Quantity = orderDetailDTO.Quantity,
+                        };
+                        _db.OrderDetails.Add(orderDetails);
+                    }
+                    _db.SaveChanges();
+                    _response.Result = order;
+                    order.OrderDetails = null;
+                    _response.StatusCode = HttpStatusCode.Created;
+                    return Ok(_response);
+                }
+            }
+            catch (Exception ex)
+            {
+                _response.IsSsuccess = false;
+                _response.ErrorMessages
+                    = new List<string> { ex.ToString() };
             }
             return _response;
         }
--- TrainPro/Utility/SD.cs
+++ TrainPro/Utility/SD.cs
@@ -5,5 +5,13 @@
         public const string SD_STORAGE_Container = "res";
         public const string Role_Admin = "admin";
         public const string Role_Customer = "Customer";
+
+
+        public const string status_pending = "Pending";
+        public const string status_confirmed = "Confirmed";
+        public const string status_beingCooked = "being cooked";
+        public const string status_readyForPickUp = "ready for pickup";
+        public const string status_Completed = "Completed";
+        public const string status_Cancelled = "Cancelled";
     }
 }
